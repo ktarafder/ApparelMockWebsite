68d735cebb29809f9bcd62a5f4baf93774f42844
ktarafder <ktarafder01@gmail.com>
2025-02-23 05:45:01 -0500

    dd

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,8 +1,8 @@
-// @ts-nocheck
 import { notFound } from 'next/navigation';
 import ProductDetail from './ProductDetail';
 import { products } from '../data';
 
+// @ts-ignore - Next.js page props type issue
 export default async function Page({ params: { id } }: { params: { id: string } }) {
   const product = await products.find(p => p.id === parseInt(id, 10));
   
